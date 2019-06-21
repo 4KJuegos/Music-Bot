79e35f104f6b6af999d11da85b20c400b15b72ca
4KJuegos <51173839+4KJuegos@users.noreply.github.com>
2019-06-21 19:04:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'NTgzMzY1MzI2MjY0MjA1MzI4.XQ0MYw.F0HMUphDjSGb3KSUj3irR7VweJo';
 
-exports.PREFIX = 'm!';
+exports.PREFIX = '!';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyB6_mvWlFBE3jYlPSeBPdlGHSaQF3ZqwuM';
